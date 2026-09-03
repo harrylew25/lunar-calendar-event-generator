cd2697bcbd91891d83a05be618d547c1eeb38358
Harry Lew <harrylew25@gmail.com>
2026-09-04 01:03:48 +0800

    [ENG-16]  feature: rework the display lunar object.

--- src/lib/wizard/preview-format.ts
+++ src/lib/wizard/preview-format.ts
@@ -1,9 +1,20 @@
 import type { GregorianDateParts } from '@lunar-dates/lunar-dates.type';
 import { Solar } from 'lunar-javascript';
 
-const getLunarDayLabelFromGregorian = (date: GregorianDateParts): string => {
-	const [year, month, day] = date;
-	return Solar.fromYmd(year, month, day).getLunar().getDayInChinese();
+type LunarDateObject = {
+	solarDate: GregorianDateParts;
+	lunarMonth: string;
+	lunarDay: string;
+	label: string;
 };
 
-export { getLunarDayLabelFromGregorian };
+export const getLunarObjectFromDate = (date: GregorianDateParts): LunarDateObject => {
+	const lunarDate = Solar.fromYmd(date[0], date[1], date[2]).getLunar();
+	const lunarMonth = lunarDate.getMonthInChinese();
+	const lunarDay = lunarDate.getDayInChinese();
+	return {
+		solarDate: date,
+		lunarMonth, lunarDay,
+		label: `${lunarMonth}月${lunarDay}日`
+	};
+};
