47abae1f6fd0753e76496cf3b6d9889053d3b1c6
Harry Lew <harrylew25@gmail.com>
2026-09-05 01:07:06 +0800

    [ENG-16] refactor: includes more data for display on the screen

--- src/lib/wizard/preview-format.ts
+++ src/lib/wizard/preview-format.ts
@@ -1,20 +1,33 @@
 import type { GregorianDateParts } from '@lunar-dates/lunar-dates.type';
-import { Solar } from 'lunar-javascript';
+import { LunarMonth, Solar } from 'lunar-javascript';
 
 type LunarDateObject = {
 	solarDate: GregorianDateParts;
 	lunarMonth: string;
 	lunarDay: string;
+	lunarYear: string;
 	label: string;
+	isLeapMonth: boolean;
+	daysInMonth: number;
 };
 
-export const getLunarObjectFromDate = (date: GregorianDateParts): LunarDateObject => {
+export const getLunarObjectFromDate = (
+	date: GregorianDateParts,
+): LunarDateObject => {
 	const lunarDate = Solar.fromYmd(date[0], date[1], date[2]).getLunar();
+	const isLeapMonth = LunarMonth.fromYm(date[0], date[1])?.isLeap() ?? false;
+	const daysInMonth = LunarMonth.fromYm(date[0], date[1])?.getDayCount() ?? 0;
 	const lunarMonth = lunarDate.getMonthInChinese();
 	const lunarDay = lunarDate.getDayInChinese();
+	const lunarYear = lunarDate.getYearInChinese();
+
 	return {
 		solarDate: date,
-		lunarMonth, lunarDay,
-		label: `${lunarMonth}月${lunarDay}日`
+		lunarMonth,
+		lunarDay,
+		lunarYear,
+		isLeapMonth,
+		daysInMonth,
+		label: `${lunarMonth}月${lunarDay}日`,
 	};
 };
