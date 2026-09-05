74627781c3f42837b9d023a6c33c0060239d57a9
Harry Lew <harrylew25@gmail.com>
2026-09-05 21:41:46 +0800

    [ENG-16] refactor(component): update the display format

--- src/components/preview/MonthCard.tsx
+++ src/components/preview/MonthCard.tsx
@@ -11,9 +11,18 @@ const sanitizeId = (id: string) => {
 	return id.trim().toLowerCase().replaceAll(' ', '-');
 };
 
+// TODO: move this to utils and add tests.
+const formatDate = (date: LunarDateNotification['date']) => {
+	const formatDateNumberToString = (value: number) =>
+		value.toString().padStart(2, '0');
+	return [...date].reverse().map(formatDateNumberToString).join('/');
+};
+
 const MonthCard = ({ monthName, events }: MonthCardProps) => {
 	return (
-		<div className="flex min-h-36 flex-col gap-3 rounded-xl border bg-card p-4 shadow-sm">
+		<article
+			id={sanitizeId(monthName)}
+			className="flex min-h-36 flex-col gap-3 rounded-xl border bg-card p-4 shadow-sm">
 			<h3 className="font-medium">{monthName}</h3>
 			<div className="flex flex-col gap-2">
 				{events.length === 0 ? (
@@ -24,19 +33,17 @@ const MonthCard = ({ monthName, events }: MonthCardProps) => {
 					))
 				)}
 			</div>
-		</div>
+		</article>
 	);
 };
 
 const EventCard = ({ event }: { event: LunarDateNotification }) => {
-	const [, gregMonth, gregDay] = event.date;
 	const lunarObj = getLunarObjectFromDate(event.date);
 	return (
 		<div
 			id={sanitizeId(event.title)}
 			className="rounded-md border bg-muted/40 px-2 py-1 text-xs">
-			{/* TODO: format the gregorian date to be more readable, append zero to single digit */}
-			{lunarObj.label}({gregMonth}/{gregDay}) - {event.title}
+			{lunarObj.label}({formatDate(event.date)}) - {event.title}
 		</div>
 	);
 };
