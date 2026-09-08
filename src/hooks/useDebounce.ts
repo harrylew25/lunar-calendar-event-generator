25ad483493ea4df5f0e9e0f6513229350bd71f19
Harry Lew <harrylew25@gmail.com>
2026-09-09 01:56:18 +0800

    [ENG-29] fix(biome lint): 2nd round fixing issues raised by the biome lint

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -3,22 +3,22 @@ import { useEffect, useState } from 'react';
 const DEFAULT_DELAY_IN_MS = 300;
 
 const useDebounce = <T>(
-    value: T,
-    delayInMs: number = DEFAULT_DELAY_IN_MS,
+	value: T,
+	delayInMs: number = DEFAULT_DELAY_IN_MS,
 ): T => {
-    const [debouncedValue, setDebouncedValue] = useState(value);
+	const [debouncedValue, setDebouncedValue] = useState(value);
 
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebouncedValue(value);
-        }, delayInMs);
+	useEffect(() => {
+		const handler = setTimeout(() => {
+			setDebouncedValue(value);
+		}, delayInMs);
 
-        return () => {
-            clearTimeout(handler);
-        };
-    }, [value, delayInMs]);
+		return () => {
+			clearTimeout(handler);
+		};
+	}, [value, delayInMs]);
 
-    return debouncedValue;
+	return debouncedValue;
 };
 
 export { useDebounce };
