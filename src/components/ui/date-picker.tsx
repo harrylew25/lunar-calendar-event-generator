d706269d2729ee028e7060db720dc764c0fa5bfe
Harry Lew <harrylew25@gmail.com>
2026-09-24 19:33:24 +0800

    [ENG-17] feat(ui): adding date picker component

---
+++ src/components/ui/date-picker.tsx
@@ -0,0 +1,110 @@
+import { format, isValid } from 'date-fns';
+import { CalendarIcon } from 'lucide-react';
+import { type ChangeEvent, type KeyboardEvent, useState } from 'react';
+import { Calendar } from '@/components/ui/calendar';
+import { Field, FieldError, FieldLabel } from '@/components/ui/field';
+import {
+	InputGroup,
+	InputGroupAddon,
+	InputGroupButton,
+	InputGroupInput,
+} from '@/components/ui/input-group';
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover';
+
+const formatDate = (date?: Date) => (date ? format(date, 'yyyy-MM-dd') : '');
+
+interface DatePickerInputProps {
+	value?: string;
+	onChange?: (value: string) => void;
+	error?: string | null;
+}
+
+const DatePickerInput = ({ value, onChange, error }: DatePickerInputProps) => {
+	const [open, setOpen] = useState(false);
+	const [date, setDate] = useState<Date | undefined>(
+		value ? new Date(value) : new Date(),
+	);
+	const [month, setMonth] = useState<Date | undefined>(date);
+	const [localValue, setLocalValue] = useState(formatDate(date));
+
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const date = new Date(e.target.value);
+		setLocalValue(e.target.value);
+		onChange?.(e.target.value);
+		if (isValid(date)) {
+			setDate(date);
+			setMonth(date);
+		}
+	};
+
+	const handleBlur = () => {
+		onChange?.(localValue);
+	};
+
+	const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'ArrowDown') {
+			e.preventDefault();
+			setOpen(true);
+		}
+	};
+
+	const handleSelect = (date: Date) => {
+		setDate(date);
+		setLocalValue(formatDate(date));
+		onChange?.(formatDate(date));
+		setOpen(false);
+	};
+
+	return (
+		<Field className="my-2" data-invalid={error ? 'true' : undefined}>
+			<FieldLabel htmlFor="date-required">Solar Date</FieldLabel>
+			<InputGroup>
+				<InputGroupInput
+					id="date-required"
+					value={localValue}
+					placeholder="YYYY-MM-DD"
+					onChange={handleChange}
+					onKeyDown={handleKeyDown}
+					onBlur={handleBlur}
+					aria-invalid={error ? 'true' : undefined}
+				/>
+				<InputGroupAddon align="inline-end">
+					<Popover open={open} onOpenChange={setOpen}>
+						<PopoverTrigger>
+							<InputGroupButton
+								id="date-picker"
+								variant="ghost"
+								size="icon-xs"
+								aria-label="Select date">
+								<CalendarIcon />
+								<span className="sr-only">Select date</span>
+							</InputGroupButton>
+						</PopoverTrigger>
+
+						<PopoverContent
+							className="w-auto overflow-hidden p-0"
+							align="end"
+							alignOffset={-8}
+							sideOffset={10}>
+							<Calendar
+								mode="single"
+								required
+								selected={date}
+								month={month}
+								onMonthChange={setMonth}
+								onSelect={handleSelect}
+							/>
+						</PopoverContent>
+					</Popover>
+				</InputGroupAddon>
+			</InputGroup>
+			{error && <FieldError>{error}</FieldError>}
+		</Field>
+	);
+};
+
+export default DatePickerInput;
